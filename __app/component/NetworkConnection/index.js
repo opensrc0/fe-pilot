6ee54451afb98693f5a1878657370bedc4f87789
swaraj <swaraj.singh@ril.com>
2024-05-06 12:01:14 +0530

    reverted code

--- __app/component/NetworkConnection/index.js
+++
@@ -1,7 +0,0 @@
-import NetworkConnection from './NetworkConnection';
-import Online from './Online';
-import Offline from './Offline';
-
-export { NetworkConnection, Online, Offline };
-
-export default NetworkConnection;
