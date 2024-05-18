1981372a4c2d2dee30884f5fec18d8c0cff9a7ff
Himanshu Gupta <himanshu12.gupta@ril.com>
2024-05-18 18:32:43 +0530

    fix(added support of multi export and services): added support of multi export and services

--- __app/component/Scanner/index.js
+++ __app/component/Scanner/index.js
@@ -2,9 +2,6 @@ export * from './Scanner';
 export * from './ScannerScanBox';
 export * from './ScannerFlash';
 export * from './ScannerFacing';
-export * from './ScannerGallery';
-export * from './ScannerCamera';
 export * from './ScannerClose';
-export * from './ScannerImageUpload';
 
 export { default } from './Scanner';
