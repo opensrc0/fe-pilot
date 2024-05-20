9cde27d31ba870db3dbafde6a2b3f69b754a558a
Himanshu Gupta <himanshu12.gupta@ril.com>
2024-05-20 18:15:59 +0530

    Replaced Network Connection component to Network Monitor

--- __app/component/NetworkConnection/Online.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Online({ children, isOnline }) {
-  return isOnline && React.Children.map(children || 'Online', (child) => React.cloneElement(typeof child === 'string' ? <span>{child}</span> : child));
-}
-
-export default Online;
