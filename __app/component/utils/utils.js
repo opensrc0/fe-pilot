f0b2eb363c52add1a64e0cc73484938973bc7ab4
Himanshu Gupta <himanshu12.gupta@ril.com>
2026-01-22 19:12:36 +0530

    docs(version update): updated can i use version

--- __app/component/utils/utils.js
+++ __app/component/utils/utils.js
@@ -6,18 +6,18 @@ const consoleColor = [
   'color: magenta',
 ];
 
-export const browserDimensions = () => ({
-  width: globalThis.document && (
-    globalThis.innerWidth
-    || globalThis.document.documentElement.clientWidth
-    || globalThis.document.body.clientWidth
-  ),
-  height: globalThis.document && (
-    globalThis.innerHeight
-    || globalThis.document.documentElement.clientHeight
-    || globalThis.document.body.clientHeight
-  ),
-});
+export const browserDimensions = () => {
+  const doc = globalThis.document;
+  if (!doc) {
+    return { width: undefined, height: undefined };
+  }
+  const docEl = doc.documentElement || {};
+  const body = doc.body || {};
+  return {
+    width: globalThis.innerWidth || docEl.clientWidth || body.clientWidth,
+    height: globalThis.innerHeight || docEl.clientHeight || body.clientHeight,
+  };
+};
 
 export const IsValidUPI = (url = '') => (url.search(/upi:\/\/pay\?pa=/) !== -1);
 
